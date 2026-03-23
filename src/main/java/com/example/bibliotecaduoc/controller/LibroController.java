b7f6cd34c183da1bb9bd67d353506932578a953e
daniel <daniel.peearce.exe@gmail.com>
2026-03-23 20:28:49 -0300

    commit

--- src/main/java/com/example/bibliotecaduoc/controller/LibroController.java
+++ src/main/java/com/example/bibliotecaduoc/controller/LibroController.java
@@ -1,8 +1,25 @@
 package com.example.bibliotecaduoc.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.bibliotecaduoc.Service.LibroService;
+import com.example.bibliotecaduoc.model.libro;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
 
 
 @Service
@@ -10,5 +27,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/api/v1/libros")
 
 public class LibroController {
+    @Autowired
+    private LibroService libroService;
+
+    @GetMapping
+    public List<libro> listaLibros(){
+        return libroService.getLibros();
+
+    }
+
+
+    @PostMapping
+    public libro agregaLibro(@RequestBody libro libro) {        
+        return libroService.saveLibro(libro);
+    }
+    
+        
+    @GetMapping("{id}")
+    public libro buscaLibro(@RequestParam int id) {
+        return libroService.getLibroId(id);
+    }
+
+    @PutMapping("{id}")
+    public libro actualizaLibro(@PathVariable int id, @RequestBody libro libro) {
+        //EL id se usara mas adelante
+        
+        return libroService.updateLibro(libro);
+
+    }
+    
+
+    @DeleteMapping("{id}")
+    public String eliminarLibro(@PathVariable int id){
+        return libroService.deleteLibro(id);
+
+    }
+    
+
+    
 
 }
